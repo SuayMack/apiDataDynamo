75ad53727603411eccb551dced9354776360ec6e
SuayMack <pry_chu@hotmail.com>
2023-10-19 22:09:37 -0300

    feat: criado modelo cliente

---
+++ src/models/cliente.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const clienteSchema = new mongoose.Schema({    
+  nome : {
+    type: String,
+    require: true
+  },
+  telefone : {
+    type: String,
+    require: true
+  },
+  email : {
+    type: String,
+    require: true,
+    unique: true
+  },
+  cnpj : {
+    type: String,
+    require: true
+  },
+  endereco : {
+    type: String,
+    require: true
+  }
+})
+
+const ClienteModel = mongoose.model('ClienteModel', clienteSchema)
+export default ClienteModel
